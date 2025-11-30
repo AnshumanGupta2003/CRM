2de739a2322cd93a443695e2588fb4ed3ea468fb
AnshumanGupta2003 <your-email@example.com>
2025-11-30 20:15:25 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+// import userRoutes from "./routes/userRoutes.js";
+import connectDB from "./config/db.js";
+
+
+dotenv.config();
+const PORT = process.env.PORT;
+
+
+const app = express();
+app.use(express.json());
+connectDB();
+
+// app.use("/api/users", userRoutes);
+
+app.listen(5000, () => console.log(`Server running on port ${PORT}`));
