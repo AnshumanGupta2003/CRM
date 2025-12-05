1a3be1bb220ad5658db26568716f3d65965c40cd
AnshumanGupta2003 <your-email@example.com>
2025-12-05 22:41:43 +0530

    editadmin

---
+++ controllers/admin/editAdmin.js
@@ -0,0 +1,24 @@
+import { successResponse, errorResponse } from "../../utils/responseHandlers.js";  
+import adminSchema from "../../models/adminModels.js";
+
+
+
+const editAdmin = async (req, res) => {
+    try {
+        const adminId = req.params.id;
+        const updateData = req.body;
+        const options = { new: true }; // To return the updated document
+
+        const updatedAdmin = await adminSchema.findByIdAndUpdate(adminId, updateData, options);
+
+        if (!updatedAdmin) {
+            return errorResponse(res, 404, "Admin not found");
+        }   
+        return successResponse(res, 200, "Admin details updated successfully", { admin: updatedAdmin });
+    } catch (error) {
+        console.error("Error updating admin details:", error);
+        return errorResponse(res, 500, "Failed to update admin details", { error: error.message });
+    }
+};
+
+export default editAdmin;
