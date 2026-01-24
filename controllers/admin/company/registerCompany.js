6c00cd312dfb2d729ab3f88b9c1f2daac5f84a8d
AnshumanGupta2003 <your-email@example.com>
2026-01-24 22:35:52 +0530

    misra ka error solve kiya 24-01 ko

--- controllers/admin/company/registerCompany.js
+++ controllers/admin/company/registerCompany.js
@@ -1,19 +1,48 @@
-import Company from '../../../models/companyModel.js';
-import { errorResponse, successResponse } from '../../../utils/responseHandlers.js';
+import Company from "../../../models/companyModel.js";
+import {
+  errorResponse,
+  successResponse,
+} from "../../../utils/responseHandlers.js";
 
-
-
-const registerCompany= async (req , res)=>{
-    try{
-    const {name , address , email , phone , city , state , country , pinCode , numberOfEmployees , registerDate , website , industry , size , owner} = req.body;
+const registerCompany = async (req, res) => {
+  try {
+    const {
+      name,
+      address,
+      email,
+      phone,
+      city,
+      state,
+      country,
+      pinCode,
+      numberOfEmployees,
+      registerDate,
+      website,
+      industry,
+      size,
+    } = req.body;
+    
     const resut = await Company.create({
-        name , address , email , phone , city , state , country , pinCode , numberOfEmployees , registerDate , website , industry , size , owner
+      name,
+      address,
+      email,
+      phone,
+      city,
+      state,
+      country,
+      pinCode,
+      numberOfEmployees,
+      registerDate,
+      website,
+      industry,
+      size,
+      owner: req.admin.id,
     });
-    return successResponse(res , 201 , "Company registered successfully" , {company:resut});
-
-}catch(error){
-errorResponse (res , 500 , "Internal Server Error" , {error: error.message});
-    }
-
-}
+    return successResponse(res, 201, "Company registered successfully", {
+      company: resut,
+    });
+  } catch (error) {
+    errorResponse(res, 500, "Internal Server Error", { error: error.message });
+  }
+};
 export default registerCompany;
--- models/companyModel.js
+++ models/companyModel.js
@@ -1,66 +1,66 @@
- 
-import mongoose from 'mongoose';
- const companySchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:true
-    }, 
-    address:{
-        type: String,
-        required:true
-    },
-    email:{
-        type:String,
-        required:true
-    },
-    phone:{
-        type:String,
-        required:true   
-    },
-    city:{
-        type:String,
-        required:true
-    },
-    state:{
-        type:String,
-        required:true   },
-    country:{
-        type:String,
-        required:true
-    },
-    pinCode:{
-        type:String,
-        required:true
-    }, 
-    numberOfEmployees:{
-        type:String,
-        required:true
-    },
-    registerDate:{
-        type:String,
-        required:true
-    },
-     website: {
+import mongoose from "mongoose";
+const companySchema = new mongoose.Schema(
+  {
+    name: {
       type: String,
-      trim: true
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    state: {
+      type: String,
+      required: true,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
+    pinCode: {
+      type: String,
+      required: true,
+    },
+    numberOfEmployees: {
+      type: String,
+      required: true,
+    },
+    registerDate: {
+      type: String,
+      required: true,
+    },
+    website: {
+      type: String,
+      trim: true,
     },
 
     industry: {
       type: String,
-      trim: true
+      trim: true,
     },
 
     size: {
       type: String,
-      enum: ["1-10", "11-50", "51-200", "201-500", "500+"]
+      enum: ["1-10", "11-50", "51-200", "201-500", "500+"],
     },
-     owner: {
+    owner: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Admin",
-      required: true
-    }}
-    ,
-    { timestamps: true }
-
-)
-export default mongoose.model('Company',companySchema);
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+export default mongoose.model("Company", companySchema);
